c947ec30927b4364d9df611e186cf859063cac09
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-04-02 14:34:50 -0300

    Download sakila db and update docker db

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+# Download sakila db
+wget http://downloads.mysql.com/docs/sakila-db.tar.gz
+tar -zxvf sakila-db.tar.gz
+
+docker exec -i mysql_container mysql -uroot -ppassword < sakila-db/sakila-schema.sql
+docker exec -i mysql_container mysql -uroot -ppassword < sakila-db/sakila-data.sql
+
 docker exec -i mysql_container mysql -uroot -ppassword < class_02.sql
