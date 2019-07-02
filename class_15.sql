7447111a303637ed26cba1a42a7ae7d0ef7e5b6f
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-07-02 19:30:25 -0300

    Update class 15

--- class_15.sql
+++ class_15.sql
@@ -32,34 +32,45 @@ CREATE OR REPLACE VIEW film_details AS
         c.name,
         f.`length`,
         f.rating,
-            (SELECT GROUP_CONCAT(a.last_name)
-                FROM film f2
-                INNER JOIN film_actor fa ON f2.film_id = fa.film_id
-                INNER JOIN actor a ON fa.actor_id = a.actor_id
-                WHERE f.film_id = f2.film_id
-                GROUP BY f2.film_id) AS 'actors'
+        GROUP_CONCAT(DISTINCT CONCAT_WS(" " ,first_name, last_name) SEPARATOR ',') AS 'actors'
     FROM film f
-        INNER JOIN film_category fc ON f.film_id = fc.film_id
-        INNER JOIN category c ON fc.category_id = c.category_id;
+        INNER JOIN film_category fc USING (film_id)
+        INNER JOIN category c USING (category_id)
+		INNER JOIN film_actor fa USING (film_id)
+		INNER JOIN actor a USING (actor_id)
+	GROUP BY 1, 4;
 
 SELECT * FROM film_details;
 
 -- 3
-SELECT * FROM sales_by_film_category;
 
-SHOW CREATE VIEW sales_by_film_category;
+-- SHOW CREATE VIEW sales_by_film_category;
+
+CREATE OR REPLACE VIEW sales_by_film_category AS
+    SELECT DISTINCT
+    	category.name,
+    	SUM(amount) as total_rental
+	FROM category
+        INNER JOIN film_category USING(category_id)
+	    INNER JOIN film USING(film_id)
+        INNER JOIN inventory USING(film_id)
+        INNER JOIN rental USING(inventory_id)
+        INNER JOIN payment USING(rental_id)
+    GROUP BY 1;
+
+SELECT * FROM sales_by_film_category;
 
 -- 4
 CREATE OR REPLACE VIEW actor_information AS
     SELECT
-        a.actor_id,
+        a.actor_id AS 'id',
         a.first_name,
         a.last_name,
-        (SELECT COUNT(fa.actor_id)
-            FROM actor a2 
-            INNER JOIN film_actor fa ON a2.actor_id = fa.actor_id
-            WHERE a.actor_id = a2.actor_id
-            GROUP BY fa.actor_id) AS "ammount_of_films"
+        (SELECT COUNT(f.film_id)
+            FROM film f 
+            INNER JOIN film_actor fa USING (film_id)
+            INNER JOIN actor a2 USING (actor_id)
+            WHERE a.actor_id = a2.actor_id) AS "ammount_of_films"
     FROM actor a;
 
 SELECT * FROM actor_information;
@@ -68,3 +79,17 @@ SELECT * FROM actor_information;
 SELECT * FROM actor_info;
 
 SHOW CREATE VIEW actor_info;
+
+-- This view fetches values from table actor, and joins them whenever it can with values from tables film_actor, film_category
+-- and category. This meaning that they will only be joined in case the actor has acted in a film.
+-- It displays the data in four columns. The first three of them are just basic actor data: its name, surname and id.
+-- The fourth column, called film_info, displays every film the actor has acted on, grouped by category.
+-- The query achieves this by concatenating every category with the group of films that belongs to it and, after that,
+-- concatenating the groups of categories with its films together.
+
+-- 6
+
+-- Materialized views are a form of chaching query results.The main difference between this type of views the ordinary ones
+-- is that Materialized views are concrete tables that store the results of a query.
+-- They are used to improve performance and exist in a variety of DBMSs, but not in MySQL. In this last scenario you could
+-- implement some workaround by using triggers or stored procedures.
